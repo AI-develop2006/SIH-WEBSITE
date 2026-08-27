f57fd94501243207edce9abe7f92719e10be2c2d
AI-develop2006 <srimaansrimaan543@gmail.com>
2026-08-27 16:30:45 +0530

    fix: add X-Login-Time to CORS allowed headers, session timeout & clear-all

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Login-Time");
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
 });
